8f6284087b6772e53a4b2556a76ea3adb7775090
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-06-28 21:25:04 -0700

    Hello world

---
+++ pern_project/server/models/products.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Products.init({
+    item_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quantity_in_stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    quantity_sold: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Products',
+    tableName: 'products',
+    timestamps: false
+  });
+  return Products;
+}
