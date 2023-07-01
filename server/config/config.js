22c1773a26aa710159ed7ddd047aa0dc8ebc4827
DanielRendon1020 <d.danny1020@gmail.com>
2023-07-01 12:47:53 -0700

    migration done

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,26 @@
-require( "dotenv" ).config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.BD_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: 'postgres',
+    password: '',
+    database: 'postgres',
+    host: 'db.tvwekwohafzwojqwkuaw.supabase.co',
     logging: true,
     dialect: "postgres"
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.BD_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: 'postgres',
+    password: '',
+    database: 'postgres',
+    host: 'db.tvwekwohafzwojqwkuaw.supabase.co',
     dialect: "postgres"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.BD_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: 'postgres',
+    password: '',
+    database: 'postgres',
+    host: 'db.tvwekwohafzwojqwkuaw.supabase.co',
     dialect: "postgres"
   }
 }
