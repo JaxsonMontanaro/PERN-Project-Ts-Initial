b98046c29f3b2852a56c21c2810332fb326f114a
Chris Wehner <chrisw7128@gmail.com>
2023-07-03 20:12:50 -0700

    added database connection to shop page, and other small improvements

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,10 +1,37 @@
-import Shoes from './shoes'
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Shoes from "./Shoes";
+import Pants from "./Pants";
+import Shirts from "./Shirts";
 
 export default function Shop() {
-    return (
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  // const fetchData = async () => {
+  // const response = await fetch("http://localhost:3000/api/products/");
+  // const json = await response.json();
+  // setProducts(json);
+  // };
+  // fetchData();
+  // }, []);
+  return (
     <div>
-        <h1>Shop</h1>
-        <Shoes />
+      <h1>Shop</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index} style={{ paddingBottom: "25px" }}>
+            <div>{product.item_id}</div>
+            <div>{product.item_name}</div>
+            <div>{product.quantity_in_stock}</div>
+            <div>{product.quantity_sold}</div>
+            <div>{product.price}</div>
+          </li>
+        ))}
+      </ul>
+      <Shoes />
+      <Pants />
+      <Shirts />
     </div>
-    );
-  }
+  );
+}
