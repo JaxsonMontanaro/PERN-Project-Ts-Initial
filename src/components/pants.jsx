37a3e528802be49b27861f7df7419fade585069a
Elliott Johnson <elliotthkj1991@gmail.com>
2023-07-05 20:28:07 -0700

    added first draft cart functionality

--- src/components/pants.jsx
+++ src/components/pants.jsx
@@ -1,51 +1,61 @@
-import { createClient } from "@supabase/supabase-js";
-import { useState } from "react";
+import { createClient } from '@supabase/supabase-js';
+import { useContext, useState } from 'react';
+import { CartContext } from '../contexts/CartContext'; // Import CartContext
 
 const supabase = createClient(
-  "https://tvwekwohafzwojqwkuaw.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2d2Vrd29oYWZ6d29qcXdrdWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgwMDg4NDAsImV4cCI6MjAwMzU4NDg0MH0.hutfQaax4HpfhD-AiORLc4027L5xIK7E64YhGFtaeNE"
+  'https://tvwekwohafzwojqwkuaw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2d2Vrd29oYWZ6d29qcXdrdWF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgwMDg4NDAsImV4cCI6MjAwMzU4NDg0MH0.hutfQaax4HpfhD-AiORLc4027L5xIK7E64YhGFtaeNE'
 );
 
 export default function Pants() {
   const [Pants, setPants] = useState({});
   const [isPantsLoading, setIsPantsLoading] = useState(true);
-  const [count, setCount] = useState(0); // State for the variable
+  const [count, setCount] = useState(0);
+  const [updatedCount, setUpdatedCount] = useState(0);
+
+  const { addToCart } = useContext(CartContext); // Access addToCart function from CartContext
 
   async function getPants() {
     const { data, error } = await supabase
-      .from("Products")
+      .from('Products')
       .select()
-      .eq("item_id", "2");
+      .eq('item_id', '2');
     setPants(data);
     setIsPantsLoading(false);
   }
 
-  const incrementCount = () => {
-    setCount(prevCount => prevCount + 1);
+  const handleInputChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+    if (!isNaN(value) && value >= 0) {
+      setUpdatedCount(value);
+    }
   };
-  const decrementCount = () => {
-    setCount(prevCount => prevCount - 1);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(updatedCount);
+    getPants();
+    addToCart(Pants[0].item_name, updatedCount); // Add selected item and quantity to cart
   };
 
   getPants();
 
   return (
     <div>
-      <h1>Type of Item: {isPantsLoading ? "loading" : Pants[0].item_name}</h1>
-      {/* <h1>Item ID: {isPantsLoading ? "loading" : Pants[0].item_id}</h1>
-      <h1>
-        Number available:{" "}
-        {isPantsLoading ? "loading" : Pants[0].quantity_in_stock}
-      </h1>
-      <h1>
-        Number previously sold:{" "}
-        {isPantsLoading ? "loading" : Pants[0].quantity_sold}
-      </h1> */}
-      <h1>Price: ${isPantsLoading ? "loading" : Pants[0].price}</h1>
-      <button onClick={incrementCount}>Increment</button>{" "}
-      <button onClick={decrementCount}>Decrement</button>{" "}
-      {/* Button to increment count */}
-      <p>Count: {count}</p> {/* Display the count */}
+      <h1>Type of Item: {isPantsLoading ? 'loading' : Pants[0].item_name}</h1>
+      <h1>Price: ${isPantsLoading ? 'loading' : Pants[0].price}</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Quantity:
+          <input
+            type='number'
+            value={updatedCount}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type='submit'>Update cart</button>
+      </form>{' '}
+      <p>Cart: {count}</p>
     </div>
   );
 }
